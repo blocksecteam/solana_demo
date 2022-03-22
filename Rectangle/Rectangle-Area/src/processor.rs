5e84b5cc24f162abea1e5fda4a38f9d66ae0c308
Yiqun Chen <chenyiqun@peckshield.com>
2022-03-22 14:10:53 +0800

    update

--- Rectangle/Rectangle-Area/src/processor.rs
+++ Rectangle/Rectangle-Area/src/processor.rs
@@ -3,7 +3,6 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult, msg,
-    borsh::{BorshDeserialize, BorshSerialize},
     pubkey::Pubkey,
     borsh::try_from_slice,
     program_error::ProgramError
