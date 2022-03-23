e4e14833dc2c65c106060b3bdb8fdf98c17319e9
Yiqun Chen <chenyiqun@peckshield.com>
2022-03-23 15:08:59 +0800

    update

--- Rectangle/Rectangle-Perimeter-Success/src/processor.rs
+++ Rectangle/Rectangle-Perimeter-Success/src/processor.rs
@@ -67,7 +67,7 @@ pub fn process_instruction(
 
     new_account_data.perimeter = new_account_data.perimeter();
 
-    let mut bw = BufWriter::new(&mut &account_data);
+    let mut bw = BufWriter::new(account_data);
     
     new_account_data.serialize(&mut bw).unwrap();
 
