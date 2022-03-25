76cbd4e40f85932106a550adce8186451f2fce79
Yiqun Chen <chenyiqun@peckshield.com>
2022-03-25 23:38:56 +0800

    update

--- Rectangle/Rectangle-Perimeter-2/src/processor.rs
+++ Rectangle/Rectangle-Perimeter-2/src/processor.rs
@@ -66,7 +66,7 @@ pub fn process_instruction(
       0 => initialize(accounts, a, b),
       1 => upgrade(accounts),
       _ => {
-        msg!(No such option)
+        msg!("No such option")
       }
    }
 
