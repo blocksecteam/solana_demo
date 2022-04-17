25032a0663bd9f3e117cccad0646fba8d04983c2
Yiqun Chen <chenyiqun@peckshield.com>
2022-04-17 20:07:13 +0800

    update

--- General-Multisig/src/client/main.ts
+++ General-Multisig/src/client/main.ts
@@ -30,16 +30,16 @@ async function main() {
   await createMultisig();
   
   // Initialize Multisig 
-  await InitializeMultisig();
+  //await InitializeMultisig();
 
   // Create Transaction 
-  await CreateTransaction();
+  //await CreateTransaction();
 
   // Approve1
-  await Approve1();
+  //await Approve1();
   
   // Approve2
-  //await Approve2();
+  await Approve2();
 
   // Execute Transaction
   await ExecuteTransaction();
