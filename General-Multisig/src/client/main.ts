31c18638d6af5cdc42a0cbff7b61a62d409b9669
Yiqun Chen <chenyiqun@peckshield.com>
2022-04-17 22:13:03 +0800

    update

--- General-Multisig/src/client/main.ts
+++ General-Multisig/src/client/main.ts
@@ -39,10 +39,10 @@ async function main() {
   //await Approve1();
   
   // Approve2
-  await Approve2();
+  //await Approve2();
 
   // Execute Transaction
-  await ExecuteTransaction();
+  //await ExecuteTransaction();
 
   console.log('Success');
 }
